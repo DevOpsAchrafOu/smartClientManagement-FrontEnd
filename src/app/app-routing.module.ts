c721cac5c2fe130b8cd05546661f13d6c85e671b
unknown <a.ouakka@distinktec.com>
2023-02-04 14:23:47 +0100

    init fontEnd angular 'ClientManagementFrontEnd'

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './components/base/layout/layout.component';
+import { HomeComponent } from './components/home/home.component';
+
+import { LoginComponent } from './components/login/login.component';
+
+
+const routes: Routes = [
+  { path: "login", component: LoginComponent },
+  // { path: "forgot-password", component: ForgotPasswordComponent },
+  // { path: 'edit-password', component: EditPasswordComponent, canActivate: [AuthGuardService]},
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  {
+    path: '', component: LayoutComponent, children: [
+      { path: '', component: HomeComponent },
+      { path: 'home', component: HomeComponent },
+
+    ]
+    // , canActivate: [AuthGuardService]
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,58 @@
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SidebarComponent } from './components/base/sidebar/sidebar.component';
+import { HeaderComponent } from './components/base/header/header.component';
+import { FooterComponent } from './components/base/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { LayoutComponent } from './components/base/layout/layout.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { NgSelect2Module } from 'ng-select2';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SidebarComponent,
+    HeaderComponent,
+    LayoutComponent,
+    FooterComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,//pour If for
+    FormsModule,//pour ngForms
+    ReactiveFormsModule,
+    NgbModule,//ng boostrap
+    ToastrModule.forRoot(),//Toastr
+    NgSelect2Module,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage : "fr",
+      loader:{//for use class "TranslateLoader" like service
+        provide : TranslateLoader,
+        useFactory :createTranslateLoader,
+        deps: [HttpClient]//argument to function  createTranslateLoader()
+      }
+    }),//Translate for i18n internationalization
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+//the files 'fr'/'ar' loader with http
+export function createTranslateLoader(http : HttpClient){
+
+  return new TranslateHttpLoader(http,"assets/i18n/",".json");//reposetory to file with extension
+}
