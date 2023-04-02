a78fa90b989c78822fd12969aeb962ab4861bff7
achrafOuakka <ouakka.achraf@gmail.com>
2023-04-02 14:55:12 +0000

    build app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl_mng: "https://smartclientmanagement-backend.onrender.com/api/mng/v1", //private
+  baseUrl_mng_login: "https://smartclientmanagement-backend.onrender.com", // public
 };
